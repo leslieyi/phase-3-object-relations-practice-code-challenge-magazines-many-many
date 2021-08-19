7dd103ff114e946cb16e31d544392f636e5d007b
leslieyi <leslieyi411@gmail.com>
2021-08-19 17:41:52 -0400

    last deliverable hard

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,22 @@
 class Magazine < ActiveRecord::Base
+    has_many :subscriptions 
+    has_many :readers, through: :subscriptions
+    
   
+    def email_list
+        self.readers.pluck(:email).join(";")
+        #self.readers.map {|reader| reader.email}.join(";")
+        #end 
+    end 
+
+#Magazine.most_popular
+# returns the Magazine instance with the most subscribers
+    def self.most_popular
+        # Lantz => # self.all.max {|a, b| a.subscriptions.length <=> b.subscriptions.length}
+        Subscription.group("magazine_id").order("COUNT(*) DESC").first.magazine
+
+        # most_popular = Subscription.group("magazine_id").order("COUNT(magazine_id) DESC").first
+        # Magazine.find(most_popular.magazine_id)
+    end 
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,20 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions 
+    has_many :magazines, through: :subscriptions
+
+# Reader#subscribe(magazine, price)
+# takes a magazine (an instance of the Magazine class) and a price (integer) as arguments, and creates a new subscription in the database associated with the magazine and the reader
+
+    def subscribe(magazine, price)
+        Subscription.create(magazine: magazine, price: price, reader: self )
+    end 
+    
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end 
+
+    def cancel_subscription(magazine)
+        self.subscriptions.find_by(magazine: magazine).destroy
+        
+    end 
 end
